9e53fd8c9c9f68a4872a04fd676249948c5abe9c
Mohamad Kittani <97052577+mkittani@users.noreply.github.com>
2024-03-02 21:19:14 +0200

    Add files via upload

---
+++ weddingPlanner/MyFeatures/event_creation.feature
@@ -0,0 +1,6 @@
+Feature: add event
+  Scenario: Event Creation
+    Given an event organizer is logged in
+    When the organizer creates a new event
+    And specifies date, time, location, theme, and description
+    Then the event is created successfully
---
+++ weddingPlanner/src/test/java/wedding/Planner/event_creation.java
@@ -0,0 +1,29 @@
+package wedding.Planner;
+
+import io.cucumber.java.en.*;
+
+public class event_creation {
+    @Given("an event organizer is logged in")
+    public void an_event_organizer_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the organizer creates a new event")
+    public void the_organizer_creates_a_new_event() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("specifies date, time, location, theme, and description")
+    public void specifies_date_time_location_theme_and_description() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the event is created successfully")
+    public void the_event_is_created_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
